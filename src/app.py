c614c803f5b23fb3c8134443e09b953b0ea5b9ab
Ellery Ricaurte <elleryrica12@gmail.com>
2023-09-27 23:36:46 -0500

    Modelo de Usuarios

--- src/app.py
+++ src/app.py
@@ -1,11 +1,15 @@
 from flask import Flask, redirect, request, jsonify, json, session, render_template
 
 from config.bd import app, db
+from models.Usuarios import Usuario, UsuarioSchema
+
+usuarios_schema = UsuarioSchema()
+usuarios_schema = UsuarioSchema(many= True)
 
 @app.route('/', methods=['GET'])
 def index():
     
-    return "Hola Ellery"
+    return render_template("login.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ src/models/Usuarios.py
@@ -0,0 +1,19 @@
+from config.bd import db, ma, app
+
+class Usuario (db.model):
+    __tablename__ = "Usuario"
+
+    id = db.Column(db.Integer, primary_key= True)
+    Email = db.Column(db.String(50), unique = True)
+    Password = db.Column(db.String(50))
+
+    def __init__ (self, Email, Password):
+        self.Email = Email
+        self.Password = Password
+
+with app.app_context():
+    db.create_all()
+
+class UsuarioSchema (ma.Schema):
+    class Meta:
+        fields = ("id", "Email", "Password")
